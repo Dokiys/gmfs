39aad37618082df6c3154e6725556aa27fdd4ce7
Dokiy <dokiychang@gmail.com>
2022-10-24 16:11:08 +0800

    feat: generate unsupported field

--- gmfs/testdata/template2.go
+++ gmfs/testdata/template2.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+type AA *time.Time
+
 // req
 type Request struct {
 	Name      string    `json:"name" binding:"required"`                                         // 名称
@@ -13,6 +15,9 @@ type Request struct {
 	StartTime time.Time `json:"start_time" binding:"required" time_format:"2006-01-02 15:04:05"` // 开始时间
 	EndTime   time.Time `json:"end_time" binding:"required" time_format:"2006-01-02 15:04:05"`   // 结束时间
 	Ids       []int     `json:"ids,omitempty"`
-	a.Condition
-	*Condition
+	StrArr    [][]string
+	MapArr    map[string][]Item
+	aaa       func() bool
+	Condition
+	*a.Condition
 }
